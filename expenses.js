6d42ae3c375b26cb8fc6cd298d2a045aa3fce67f
Joël Birrer <birrer.joel@hotmail.ch>
2020-07-19 21:12:34 +0200

    added to website

--- expenses.js
+++ expenses.js
@@ -1,3 +1,4 @@
+
 let grosi  = [1500, 750, 50, 1100, 2000, 300, 40, 64, 180];
   
 let steffi = [1450,135, 342, 80, 540, 160];
@@ -43,27 +44,57 @@ console.log("Durchschnitt in Franken = " + avg);
 let get =0;
 let givex =0;
 let givey =0;
+let b;
+let a;
+let c;
 
 if (grositot > steffitot && grositot > tanjatot){
   get = grositot - avg;
-  console.log("Grosi bekommt " + get + " Franken");
+  b = "Grosi bekommt " + get + " Franken";
+  console.log(b);
   givex = avg - steffitot;
   givey = avg - tanjatot;
-  console.log(givex + " von Steffi, und von Tanja "+ givey);
+  a = givex + " von Steffi, und von Tanja "+ givey;
+  console.log(a);
 }
 else
 if (steffitot > grositot && steffitot > tanjatot){
   get = steffitot -avg;
-  console.log("Steffi bekommt " + get + " Franken");
+  b = "Steffi bekommt " + get + " Franken";
+  console.log(b);
   givex = avg - grositot;
   givey = avg - tanjatot;
-  console.log(givex + " von Grosi, und von Tanja "+ givey);
+   a = givex + " von Grosi, und von Tanja "+ givey;
+   console.log(a);
 }
 else{get = tanjatot - avg;
-  console.log("Tanja bekommnt " + get + " Franken");
+   b = "Tanja bekommt " + get + " Franken";
+   console.log(b);
   givex = avg - steffitot;
+  
   givey = avg - grositot;
-  console.log(givex + " von Steffi, und von Grosi "+ givey);
+    if (givey < 0) {
+      c = avg-steffitot - (-1)*(avg-grositot);
+      a = "Tanja von Steffi: " + get + " und Grosi von Steffi: " + (givex-c);
+      console.log(a);
+    }
+    else {
+   a = givex + " von Steffi, und von Grosi "+ givey;
+   console.log(a);}
+  
 }
+
+document.getElementById("main").innerHTML = "Durchschnitt in Franken: " + Math.round(tot/3);
+
+document.getElementById("juhu").innerHTML = grosi;
+document.getElementById("steff").innerHTML = steffi;
+document.getElementById("tan").innerHTML = tanja;
+
+document.getElementById("grosiTot").innerHTML = `${"Grosi:"} ${grositot} ${"in Franken und in Kuna: "} ${Math.round(grositot/.14)} `;
+document.getElementById("steffiTot").innerHTML = `${"Steffi:"} ${steffitot} ${"in Franken und in Kuna: "} ${Math.round(steffitot/.14)} `;
+document.getElementById("tanjaTot").innerHTML = `${"Tanja:"} ${tanjatot} ${"in Franken und in Kuna: "} ${Math.round(tanjatot/.14)} `;
+
+document.getElementById("cost").innerHTML = b;
+document.getElementById("costt").innerHTML = a;
 
 
